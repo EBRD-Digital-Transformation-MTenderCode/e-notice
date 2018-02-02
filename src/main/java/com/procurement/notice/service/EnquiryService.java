541b3612bb1c2f9198a75332ac2fe429c5cb3b94
andreykolchev <andreykolchev@gmail.com>
2018-02-02 18:10:13 +0200

    add ein, fs

---
+++ src/main/java/com/procurement/notice/service/EnquiryService.java
@@ -0,0 +1,15 @@
+package com.procurement.notice.service;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.procurement.notice.model.bpe.ResponseDto;
+import java.time.LocalDateTime;
+import org.springframework.stereotype.Service;
+
+@Service
+public interface EnquiryService {
+
+    ResponseDto createEnquiry(String cpid,
+                         String ocid,
+                         String stage,
+                         JsonNode data);
+}
---
+++ src/main/java/com/procurement/notice/service/EnquiryServiceImpl.java
@@ -0,0 +1,11 @@
+package com.procurement.notice.service;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.procurement.notice.model.bpe.ResponseDto;
+
+public class EnquiryServiceImpl implements EnquiryService {
+    @Override
+    public ResponseDto createEnquiry(String cpid, String ocid, String stage, JsonNode data) {
+        return null;
+    }
+}
