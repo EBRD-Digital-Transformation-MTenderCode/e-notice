28416f1afa614a2c7d3fcd357885334459a44bd0
andreykolchev <andreykolchev@gmail.com>
2018-03-10 16:18:10 +0200

    start new stage

--- src/main/java/com/procurement/notice/controller/MainController.java
+++ src/main/java/com/procurement/notice/controller/MainController.java
@@ -27,7 +27,7 @@ public class MainController {
     public ResponseEntity<ResponseDto> createRelease(@RequestParam final String cpId,
                                                      @RequestParam(required = false) final String ocId,
                                                      @RequestParam final String stage,
-                                                     @RequestParam final String previousStage,
+                                                     @RequestParam(required = false) final String previousStage,
                                                      @RequestParam final String operation,
                                                      @RequestParam(required = false) final String phase,
                                                      @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
