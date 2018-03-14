afd4cf1cd9467acaff1d515a8ab68bb2eeb6b827
andreykolchev <andreykolchev@gmail.com>
2018-03-14 14:29:11 +0200

    budget parties

--- src/main/java/com/procurement/notice/model/budget/FsBudget.java
+++ src/main/java/com/procurement/notice/model/budget/FsBudget.java
@@ -4,12 +4,10 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
-import com.procurement.notice.model.ocds.BudgetBreakdown;
-import com.procurement.notice.model.ocds.EuropeanUnionFunding;
-import com.procurement.notice.model.ocds.Period;
-import com.procurement.notice.model.ocds.Value;
+import com.procurement.notice.model.ocds.*;
 import java.util.List;
 import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 import org.apache.commons.lang.builder.EqualsBuilder;
@@ -23,14 +21,11 @@ import org.apache.commons.lang.builder.HashCodeBuilder;
         "description",
         "period",
         "amount",
-        "project",
-        "projectID",
-        "uri",
-        "source",
         "europeanUnionFunding",
         "isEuropeanUnionFunded",
         "budgetBreakdown",
-        "verified"
+        "verified",
+        "sourceEntity"
 })
 public class FsBudget {
     @JsonProperty("id")
@@ -40,89 +35,34 @@ public class FsBudget {
     @JsonProperty("period")
     private final Period period;
     @JsonProperty("amount")
-    @Valid
     private final Value amount;
-    @JsonProperty("project")
-    private final String project;
-    @JsonProperty("projectID")
-    private final String projectID;
-    @JsonProperty("uri")
-    private final String uri;
-    @JsonProperty("source")
-    private final String source;
     @JsonProperty("europeanUnionFunding")
     @Valid
     private final EuropeanUnionFunding europeanUnionFunding;
     @JsonProperty("isEuropeanUnionFunded")
     private final Boolean isEuropeanUnionFunded;
-    @JsonProperty("budgetBreakdown")
-    private final List<BudgetBreakdown> budgetBreakdown;
     @JsonProperty("verified")
-    private final Boolean verified;
+    private Boolean verified;
+    @JsonProperty("sourceEntity")
+    @Valid
+    private OrganizationReference sourceEntity;
 
     @JsonCreator
     public FsBudget(@JsonProperty("id") final String id,
-                    @JsonProperty("description") final String description,
-                    @JsonProperty("period") final Period period,
-                    @JsonProperty("amount") final Value amount,
-                    @JsonProperty("project") final String project,
-                    @JsonProperty("projectID") final String projectID,
-                    @JsonProperty("uri") final String uri,
-                    @JsonProperty("source") final String source,
-                    @JsonProperty("europeanUnionFunding") final EuropeanUnionFunding europeanUnionFunding,
-                    @JsonProperty("isEuropeanUnionFunded") final Boolean isEuropeanUnionFunded,
-                    @JsonProperty("budgetBreakdown") final List<BudgetBreakdown> budgetBreakdown,
-                    @JsonProperty("verified") final Boolean verified) {
+                       @JsonProperty("description") final String description,
+                       @JsonProperty("period") final Period period,
+                       @JsonProperty("amount") final Value amount,
+                       @JsonProperty("europeanUnionFunding") final EuropeanUnionFunding europeanUnionFunding,
+                       @JsonProperty("isEuropeanUnionFunded") final Boolean isEuropeanUnionFunded,
+                       @JsonProperty("verified") final Boolean verified,
+                       @JsonProperty("sourceEntity") final OrganizationReference sourceEntity) {
         this.id = id;
         this.description = description;
         this.period = period;
         this.amount = amount;
-        this.project = project;
-        this.projectID = projectID;
-        this.uri = uri;
-        this.source = source;
         this.europeanUnionFunding = europeanUnionFunding;
         this.isEuropeanUnionFunded = isEuropeanUnionFunded;
-        this.budgetBreakdown = budgetBreakdown;
-        this.verified = verified;
-    }
-
-    @Override
-    public int hashCode() {
-        return new HashCodeBuilder().append(id)
-                .append(description)
-                .append(amount)
-                .append(project)
-                .append(projectID)
-                .append(uri)
-                .append(source)
-                .append(europeanUnionFunding)
-                .append(isEuropeanUnionFunded)
-                .append(budgetBreakdown)
-                .append(verified)
-                .toHashCode();
-    }
-
-    @Override
-    public boolean equals(final Object other) {
-        if (other == this) {
-            return true;
-        }
-        if (!(other instanceof FsBudget)) {
-            return false;
-        }
-        final FsBudget rhs = (FsBudget) other;
-        return new EqualsBuilder().append(id, rhs.id)
-                .append(description, rhs.description)
-                .append(amount, rhs.amount)
-                .append(project, rhs.project)
-                .append(projectID, rhs.projectID)
-                .append(uri, rhs.uri)
-                .append(source, rhs.source)
-                .append(europeanUnionFunding, rhs.europeanUnionFunding)
-                .append(isEuropeanUnionFunded, rhs.isEuropeanUnionFunded)
-                .append(budgetBreakdown, rhs.budgetBreakdown)
-                .append(verified, rhs.verified)
-                .isEquals();
+        this.verified = verified == null ? false : verified;
+        this.sourceEntity = sourceEntity;
     }
 }
