2e73a261f045e6a9354e66ab8ac3021d7cbee502
andreykolchev <andreykolchev@gmail.com>
2018-04-26 14:57:07 +0300

    suspend tender dto

--- src/main/kotlin/com/procurement/notice/model/tender/dto/TenderDto.kt
+++ src/main/kotlin/com/procurement/notice/model/tender/dto/TenderDto.kt
@@ -2,20 +2,16 @@ package com.procurement.notice.model.tender.dto
 
 import com.fasterxml.jackson.annotation.JsonProperty
 import com.procurement.notice.model.ocds.Period
-import com.procurement.notice.model.ocds.TenderStatus
 import com.procurement.notice.model.ocds.TenderStatusDetails
 
 data class TenderDto(
-
-        @JsonProperty("status")
-        val status: TenderStatus,
 
         @JsonProperty("statusDetails")
         val statusDetails: TenderStatusDetails,
 
         @JsonProperty("tenderPeriod")
-        val tenderPeriod: Period,
+        val tenderPeriod: Period?,
 
         @JsonProperty("enquiryPeriod")
-        val enquiryPeriod: Period
+        val enquiryPeriod: Period?
 )
