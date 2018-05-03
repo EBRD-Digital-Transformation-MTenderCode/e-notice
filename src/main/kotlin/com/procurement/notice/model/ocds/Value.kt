193c105fd854450a0eba09a62d9305707a7dd56e
andreykolchev <andreykolchev@gmail.com>
2018-05-03 18:14:41 +0300

    MoneyDeserializer::class

--- src/main/kotlin/com/procurement/notice/model/ocds/Value.kt
+++ src/main/kotlin/com/procurement/notice/model/ocds/Value.kt
@@ -3,12 +3,15 @@ package com.procurement.notice.model.ocds
 import com.fasterxml.jackson.annotation.JsonInclude
 import com.fasterxml.jackson.annotation.JsonProperty
 import com.fasterxml.jackson.annotation.JsonPropertyOrder
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize
+import com.procurement.notice.databinding.MoneyDeserializer
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @JsonPropertyOrder("amount", "currency")
 data class Value(
 
         @JsonProperty("amount")
+        @JsonDeserialize(using = MoneyDeserializer::class)
         var amount: Double?,
 
         @JsonProperty("currency")
