199efedea513de9e7430394df07f741af15d911e
andreykolchev <andreykolchev@gmail.com>
2018-09-21 16:36:58 +0300

    history

--- src/main/kotlin/com/procurement/notice/NoticeApplication.kt
+++ src/main/kotlin/com/procurement/notice/NoticeApplication.kt
@@ -3,10 +3,8 @@ package com.procurement.notice
 import com.procurement.notice.config.ApplicationConfig
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient
 
 @SpringBootApplication(scanBasePackageClasses = [ApplicationConfig::class])
-@EnableEurekaClient
 class NoticeApplication
 
 fun main(args: Array<String>) {
