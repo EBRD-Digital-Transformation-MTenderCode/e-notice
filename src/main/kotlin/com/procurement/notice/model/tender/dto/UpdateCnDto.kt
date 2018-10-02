e886674615b0b626be70c56c1ee7b88d065d2cc2
andreykolchev <andreykolchev@gmail.com>
2018-10-02 18:27:26 +0300

    ES-1037

--- src/main/kotlin/com/procurement/notice/model/tender/dto/UpdateCnDto.kt
+++ src/main/kotlin/com/procurement/notice/model/tender/dto/UpdateCnDto.kt
@@ -1,12 +1,13 @@
 package com.procurement.notice.model.tender.dto
 
 import com.fasterxml.jackson.annotation.JsonCreator
-import com.fasterxml.jackson.annotation.JsonInclude
-import com.procurement.notice.model.ocds.Amendment
-import com.procurement.notice.model.tender.enquiry.RecordEnquiry
 
-@JsonInclude(JsonInclude.Include.NON_NULL)
 data class UpdateCnDto @JsonCreator constructor(
 
-        var amendments: List<Amendment>
+        val amendment: AmendmentUpdateCn?
+)
+
+data class AmendmentUpdateCn @JsonCreator constructor(
+
+        val relatedLots: Set<String>
 )
