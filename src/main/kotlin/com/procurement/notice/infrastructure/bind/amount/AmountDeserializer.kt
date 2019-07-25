1308fc1bc5ae33691ab8d2a737b13491377f8e6e
Maxim Sambulat <maxim.sambulat@gmail.com>
2019-07-25 17:32:11 +0300

    Added checking to deserializer that the type of amount attribute and quantity attribute is a real number.

--- src/main/kotlin/com/procurement/notice/infrastructure/bind/amount/AmountDeserializer.kt
+++ src/main/kotlin/com/procurement/notice/infrastructure/bind/amount/AmountDeserializer.kt
@@ -2,6 +2,7 @@ package com.procurement.notice.infrastructure.bind.amount
 
 import com.fasterxml.jackson.core.JsonParser
 import com.fasterxml.jackson.core.JsonProcessingException
+import com.fasterxml.jackson.core.JsonToken
 import com.fasterxml.jackson.databind.DeserializationContext
 import com.fasterxml.jackson.databind.JsonDeserializer
 import com.procurement.notice.infrastructure.exception.AmountValueException
@@ -18,6 +19,13 @@ class AmountDeserializer : JsonDeserializer<BigDecimal>() {
     }
 
     @Throws(IOException::class, JsonProcessingException::class)
-    override fun deserialize(jsonParser: JsonParser, deserializationContext: DeserializationContext): BigDecimal =
-        deserialize(jsonParser.text)
+    override fun deserialize(jsonParser: JsonParser, deserializationContext: DeserializationContext): BigDecimal {
+        if (jsonParser.currentToken != JsonToken.VALUE_NUMBER_FLOAT && jsonParser.currentToken != JsonToken.VALUE_NUMBER_INT) {
+            throw AmountValueException(
+                amount = "\"${jsonParser.text}\"",
+                description = "The value must be a real number."
+            )
+        }
+        return deserialize(jsonParser.text)
+    }
 }
