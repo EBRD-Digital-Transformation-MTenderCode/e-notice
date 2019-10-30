5315def581ff5134a6e4bcce8ec5b00a1d8d96cf
Maxim Sambulat <maxim.sambulat@gmail.com>
2019-10-30 16:09:48 +0200

    ES-2020 Changed process creating a release for 'updateCn' operation type.

--- src/main/kotlin/com/procurement/notice/service/UpdateReleaseService.kt
+++ src/main/kotlin/com/procurement/notice/service/UpdateReleaseService.kt
@@ -14,77 +14,125 @@ import java.util.*
 @Service
 class UpdateReleaseService(private val releaseService: ReleaseService) {
 
-    fun updateCn(cpid: String,
-                 ocid: String,
-                 stage: String,
-                 releaseDate: LocalDateTime,
-                 isAuction: Boolean,
-                 data: JsonNode): ResponseDto {
-        val msReq = releaseService.getMs(data)
-        val recordTender = releaseService.getRecordTender(data)
-        val msEntity = releaseService.getMsEntity(cpid)
-        val ms = releaseService.getMs(msEntity.jsonData)
+    fun updateCn(
+        cpid: String,
+        ocid: String,
+        stage: String,
+        releaseDate: LocalDateTime,
+        isAuction: Boolean,
+        data: JsonNode
+    ): ResponseDto {
         val dto = toObject(UpdateCnDto::class.java, data)
-        msReq.tender.apply {
-            id = ms.tender.id
-            status = ms.tender.status
-            statusDetails = ms.tender.statusDetails
-            hasEnquiries = ms.tender.hasEnquiries
-            procuringEntity = ms.tender.procuringEntity
-        }
-        ms.apply {
-            id = releaseService.newReleaseId(cpid)
-            date = releaseDate
-            planning = msReq.planning
-            tender = msReq.tender
-        }
+        val requestTenderEV = releaseService.getRecordTender(data)
+
+        val requestMS = releaseService.getMs(data)
+        val msEntity = releaseService.getMsEntity(cpid)
+        val recordMS = releaseService.getMs(msEntity.jsonData)
+
+        val updatedRecordMS = recordMS.copy(
+            id = releaseService.newReleaseId(cpid), //FR-5.0.1
+            date = releaseDate, //FR-5.0.2
+            tag = listOf(Tag.COMPILED), //FR-MR-5.5.2.3.1
+            planning = requestMS.planning, //FR-MR-5.5.2.3.6
+            tender = requestMS.tender.copy(
+                id = recordMS.tender.id, //FR-MR-5.5.2.3.4
+                status = recordMS.tender.status, //FR-MR-5.5.2.3.4
+                statusDetails = recordMS.tender.statusDetails, //FR-MR-5.5.2.3.4
+                hasEnquiries = recordMS.tender.hasEnquiries,
+                procuringEntity = recordMS.tender.procuringEntity //FR-MR-5.5.2.3.3
+            ),
+
+            //FR-MR-5.5.2.3.7
+            parties = recordMS.parties?.map { party ->
+                if (party.id == requestMS.tender.procuringEntity!!.id)
+                    party.copy(
+                        persones = requestMS.tender.procuringEntity!!.persones
+                    )
+                else
+                    party
+            }?.toHashSet()
+        )
+
         val recordEntity = releaseService.getRecordEntity(cpId = cpid, ocId = ocid)
-        val record = releaseService.getRecord(recordEntity.jsonData)
-        val isAuctionPeriodChanged = dto.isAuctionPeriodChanged ?: false
-        recordTender.apply {
-            title = record.tender.title
-            description = record.tender.description
-            enquiries = record.tender.enquiries
-            hasEnquiries = record.tender.hasEnquiries
-            if (isAuction && !isAuctionPeriodChanged) {
-                auctionPeriod = record.tender.auctionPeriod
-                procurementMethodModalities = record.tender.procurementMethodModalities
-                electronicAuctions = record.tender.electronicAuctions
-            }
-        }
-        val actualReleaseID = record.id
+        val recordEV = releaseService.getRecord(recordEntity.jsonData)
+
+        val actualReleaseID = recordEV.id
         val newReleaseID = releaseService.newReleaseId(ocid)
-        val amendments = record.tender.amendments?.toMutableList() ?: mutableListOf()
-        var rationale: String
-        val canceledLots = dto.amendment?.relatedLots
-        rationale = if (canceledLots != null) {
-            "Changing of Contract Notice due to the need of cancelling lot / lots"
-        } else {
-            "General change of Contract Notice"
-        }
-        val newAmendmentId = UUID.randomUUID().toString()
-        amendments.add(Amendment(
-                id = newAmendmentId,
-                amendsReleaseID = actualReleaseID,
-                releaseID = newReleaseID,
-                date = releaseDate,
-                relatedLots = canceledLots,
-                rationale = rationale,
-                changes = null,
-                description = null,
-                documents = null
-        ))
-        record.apply {
-            /* previous record*/
-            id = newReleaseID
-            date = releaseDate
-            tag = listOf(Tag.TENDER_AMENDMENT)
-            tender = recordTender
-            tender.amendments = if (amendments.isNotEmpty()) amendments else null
-        }
-        releaseService.saveMs(cpId = cpid, ms = ms, publishDate = msEntity.publishDate)
-        releaseService.saveRecord(cpId = cpid, stage = stage, record = record, publishDate = recordEntity.publishDate)
-        return ResponseDto(data = DataResponseDto(cpid = cpid, ocid = ocid, amendmentsIds = listOf(newAmendmentId)))
+        val requestAmendments: List<Amendment> = requestTenderEV.amendments ?: emptyList()
+        val newAmendments: List<Amendment> = if (requestAmendments.isNotEmpty())
+            requestAmendments.map { amendment ->
+                Amendment(
+                    id = UUID.randomUUID().toString(),
+                    amendsReleaseID = actualReleaseID,
+                    releaseID = newReleaseID,
+                    date = releaseDate,
+                    rationale = "Changing of Contract Notice due to the need of cancelling lot / lots",
+                    relatedLots = amendment.relatedLots,
+                    changes = null,
+                    description = null,
+                    documents = null
+                )
+            }
+        else
+            listOf(
+                Amendment(
+                    id = UUID.randomUUID().toString(),
+                    amendsReleaseID = actualReleaseID,
+                    releaseID = newReleaseID,
+                    date = releaseDate,
+                    rationale = "General change of Contract Notice",
+                    relatedLots = null,
+                    changes = null,
+                    description = null,
+                    documents = null
+                )
+            )
+
+        val recordEVAmendments = recordEV.tender.amendments ?: emptyList()
+        val updatedAmendments: List<Amendment> = recordEVAmendments.plus(newAmendments)
+
+        val isAuctionPeriodChanged = dto.isAuctionPeriodChanged ?: false
+        val updatedRecordEV = recordEV.copy(
+            id = newReleaseID, //FR-5.0.1
+            date = releaseDate, //FR-5.0.2
+            tag = listOf(Tag.TENDER_AMENDMENT), //FR-ER-5.5.2.3.1
+            tender = requestTenderEV.copy(
+                title = recordEV.tender.title, //FR-ER-5.5.2.3.3
+                description = recordEV.tender.description, //FR-ER-5.5.2.3.3
+                enquiries = recordEV.tender.enquiries, //FR-ER-5.5.2.3.3
+                hasEnquiries = recordEV.tender.hasEnquiries, //FR-ER-5.5.2.3.3
+                amendments = updatedAmendments, //if (amendments.isNotEmpty()) amendments else null //FR-ER-5.5.2.3.4
+                auctionPeriod = if (isAuction && isAuctionPeriodChanged)
+                    requestTenderEV.auctionPeriod
+                else
+                    recordEV.tender.auctionPeriod,
+
+                procurementMethodModalities = if (isAuction && isAuctionPeriodChanged)
+                    requestTenderEV.procurementMethodModalities
+                else
+                    recordEV.tender.procurementMethodModalities,
+
+                electronicAuctions = if (isAuction && isAuctionPeriodChanged)
+                    requestTenderEV.electronicAuctions
+                else
+                    recordEV.tender.electronicAuctions
+            )
+        )
+
+        releaseService.saveMs(cpId = cpid, ms = updatedRecordMS, publishDate = msEntity.publishDate)
+        releaseService.saveRecord(
+            cpId = cpid,
+            stage = stage,
+            record = updatedRecordEV,
+            publishDate = recordEntity.publishDate
+        )
+        return ResponseDto(
+            data = DataResponseDto(
+                cpid = cpid,
+                ocid = ocid,
+                amendmentsIds = newAmendments.map { it.id!! }
+            )
+        )
     }
 
     fun updatePn(cpid: String,
