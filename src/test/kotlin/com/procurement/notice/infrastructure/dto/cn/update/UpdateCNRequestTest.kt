1709abbc1a04d1751227d97bbc3b1274f0df1314
Maxim Sambulat <maxim.sambulat@gmail.com>
2019-11-05 11:53:49 +0200

    ES-2128 Added request DTO for 'updateCn' operation type.

---
+++ src/test/kotlin/com/procurement/notice/infrastructure/dto/cn/update/UpdateCNRequestTest.kt
@@ -0,0 +1,32 @@
+package com.procurement.notice.infrastructure.dto.cn.update
+
+import com.procurement.notice.infrastructure.AbstractDTOTestBase
+import org.junit.jupiter.api.Test
+
+class UpdateCNRequestTest : AbstractDTOTestBase<UpdateCNRequest>(UpdateCNRequest::class.java) {
+
+    @Test
+    fun fully() {
+        testBindingAndMapping("json/dto/cn/update/request_cn_update_full.json")
+    }
+
+    @Test
+    fun required1() {
+        testBindingAndMapping("json/dto/cn/update/request_cn_update_required_1.json")
+    }
+
+    @Test
+    fun required2() {
+        testBindingAndMapping("json/dto/cn/update/request_cn_update_required_2.json")
+    }
+
+    @Test
+    fun required3() {
+        testBindingAndMapping("json/dto/cn/update/request_cn_update_required_3.json")
+    }
+//
+//    @Test
+//    fun required2() {
+//        testBindingAndMapping("json/dto/award/create/request_crate_award_required_2.json")
+//    }
+}
