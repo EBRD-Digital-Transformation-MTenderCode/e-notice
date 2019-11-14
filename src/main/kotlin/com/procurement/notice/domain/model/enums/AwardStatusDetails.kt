1ebaac2d1dc857a57deb9a98fde06fce2caaec83
Maxim Sambulat <maxim.sambulat@gmail.com>
2019-11-14 14:31:59 +0200

    ES-2164 Added new 'AwardStatusDetails' enum.

---
+++ src/main/kotlin/com/procurement/notice/domain/model/enums/AwardStatusDetails.kt
@@ -0,0 +1,25 @@
+package com.procurement.notice.domain.model.enums
+
+import com.fasterxml.jackson.annotation.JsonValue
+import com.procurement.notice.exception.EnumException
+
+enum class AwardStatusDetails(@JsonValue val value: String) {
+    ACTIVE("active"),
+    EMPTY("empty"),
+    UNSUCCESSFUL("unsuccessful"),
+    CONSIDERATION("consideration"),
+    AWAITING("awaiting");
+
+    override fun toString(): String = value
+
+    companion object {
+        private val elements: Map<String, AwardStatusDetails> = values().associateBy { it.value.toUpperCase() }
+
+        fun fromString(value: String): AwardStatusDetails = elements[value.toUpperCase()]
+            ?: throw EnumException(
+                enumType = AwardStatusDetails::class.java.canonicalName,
+                value = value,
+                values = values().joinToString { it.value }
+            )
+    }
+}
