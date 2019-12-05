a87c1506fe657d674c5764b2ce5bc8c4ee85c4ac
katherine <riodael@gmail.com>
2019-12-05 12:31:20 +0200

    ES-2319 Changed EvaluateAwardData DTO

--- src/main/kotlin/com/procurement/notice/application/service/award/EvaluateAwardData.kt
+++ src/main/kotlin/com/procurement/notice/application/service/award/EvaluateAwardData.kt
@@ -1,25 +1,35 @@
 package com.procurement.notice.application.service.award
 
-import java.math.BigDecimal
+import com.procurement.notice.domain.model.award.AwardId
+import com.procurement.notice.domain.model.bid.BidId
+import com.procurement.notice.domain.model.enums.AwardStatus
+import com.procurement.notice.domain.model.enums.AwardStatusDetails
+import com.procurement.notice.domain.model.enums.BidDocumentType
+import com.procurement.notice.domain.model.lot.LotId
+import com.procurement.notice.domain.model.money.Money
 import java.time.LocalDateTime
-import java.util.*
 
-class EvaluateAwardData(val award: Award) {
+class EvaluateAwardData(
+    val award: Award,
+    val nextAwardForUpdate: NextAwardForUpdate?,
+    val bid: Bid?
+) {
     data class Award(
-        val id: String,
+        val id: AwardId,
         val date: LocalDateTime,
         val description: String?,
-        val status: String,
-        val statusDetails: String,
-        val relatedLots: List<UUID>,
-        val value: Value,
+        val status: AwardStatus,
+        val statusDetails: AwardStatusDetails,
+        val relatedLots: List<LotId>,
+        val relatedBid: BidId,
+        val value: Money,
         val suppliers: List<Supplier>,
-        val documents: List<Document>?
+        val documents: List<Document>
     ) {
-
-        data class Value(val amount: BigDecimal, val currency: String)
-
-        data class Supplier(val id: String, val name: String)
+        data class Supplier(
+            val id: String,
+            val name: String
+        )
 
         data class Document(
             val documentType: String,
@@ -28,7 +38,27 @@ class EvaluateAwardData(val award: Award) {
             val url: String,
             val title: String?,
             val description: String?,
-            val relatedLots: List<UUID>?
+            val relatedLots: List<LotId>
+        )
+    }
+
+    data class NextAwardForUpdate(
+        val id: AwardId,
+        val statusDetails: AwardStatusDetails
+    )
+
+    data class Bid(
+        val id: BidId,
+        val documents: List<Document>
+    ) {
+        data class Document(
+            val documentType: BidDocumentType,
+            val id: String,
+            val datePublished: LocalDateTime,
+            val url: String,
+            val title: String?,
+            val description: String?,
+            val relatedLots: List<LotId>
         )
     }
 }
