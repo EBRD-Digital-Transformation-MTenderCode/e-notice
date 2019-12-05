4cbd3bd2e6ed9d873a8ac0f3a46e3a9268725535
katherine <riodael@gmail.com>
2019-12-05 13:22:36 +0200

    ES-2319 Added converter from request to data for EvaluateAward

--- src/main/kotlin/com/procurement/notice/service/CommandService.kt
+++ src/main/kotlin/com/procurement/notice/service/CommandService.kt
@@ -6,7 +6,6 @@ import com.procurement.notice.application.service.award.CreateAwardData
 import com.procurement.notice.application.service.award.EndAwardPeriodContext
 import com.procurement.notice.application.service.award.EndAwardPeriodData
 import com.procurement.notice.application.service.award.EvaluateAwardContext
-import com.procurement.notice.application.service.award.EvaluateAwardData
 import com.procurement.notice.application.service.award.StartAwardPeriodContext
 import com.procurement.notice.application.service.award.StartAwardPeriodData
 import com.procurement.notice.application.service.award.auction.StartAwardPeriodAuctionContext
@@ -1245,41 +1244,7 @@ class CommandService(
                     startDate = cm.startDate
                 )
                 val request = toObject(EvaluateAwardRequest::class.java, cm.data)
-                val updateAwardData = EvaluateAwardData(
-                    award = request.award.let { award ->
-                        EvaluateAwardData.Award(
-                            id = award.id,
-                            date = award.date,
-                            description = award.description,
-                            status = award.status,
-                            statusDetails = award.statusDetails,
-                            relatedLots = award.relatedLots.toList(),
-                            value = award.value.let { value ->
-                                EvaluateAwardData.Award.Value(
-                                    amount = value.amount,
-                                    currency = value.currency
-                                )
-                            },
-                            suppliers = award.suppliers.map { supplier ->
-                                EvaluateAwardData.Award.Supplier(
-                                    id = supplier.id,
-                                    name = supplier.name
-                                )
-                            },
-                            documents = award.documents?.map { document ->
-                                EvaluateAwardData.Award.Document(
-                                    documentType = document.documentType,
-                                    id = document.id,
-                                    datePublished = document.datePublished,
-                                    url = document.url,
-                                    title = document.title,
-                                    description = document.description,
-                                    relatedLots = document.relatedLots
-                                )
-                            }
-                        )
-                    }
-                )
+                val updateAwardData = request.convert()
                 awardService.evaluate(context = updateAwardContext, data = updateAwardData)
                 ResponseDto(
                     data = DataResponseDto(
