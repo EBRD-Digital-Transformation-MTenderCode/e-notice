a9ec168e8a61826963897ba2656cfb183151bd7a
yevhenii <yevheniiskoryi@gmail.com>
2019-12-26 12:27:30 +0200

    ES-2435 extended RequirementValue class with NoneValue

--- src/main/kotlin/com/procurement/notice/model/ocds/Requirement.kt
+++ src/main/kotlin/com/procurement/notice/model/ocds/Requirement.kt
@@ -10,15 +10,13 @@ class Requirement(
     val description: String?,
     val period: Period?,
     val dataType: RequirementDataType,
-    val value: RequirementValue?
+    val value: RequirementValue
 ) {
     data class Period(
         val startDate: LocalDateTime,
         val endDate: LocalDateTime
     )
-
 }
-
 
 sealed class RequirementValue
 
@@ -74,3 +72,5 @@ sealed class RangeValue : RequirementValue() {
     data class AsNumber(val minValue: BigDecimal, val maxValue: BigDecimal) : RangeValue()
     data class AsInteger(val minValue: Long, val maxValue: Long) : RangeValue()
 }
+
+object NoneValue : RequirementValue()
