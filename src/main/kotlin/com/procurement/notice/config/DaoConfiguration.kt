5dd4d4656cd5e26f7f8074ce64c0764e19c7e4ee
Evgen Kopach <zkopach@gmail.com>
2020-02-07 15:57:10 +0200

    ES-2520 Moved CassandraProperties to properties directory

--- src/main/kotlin/com/procurement/notice/config/DaoConfiguration.kt
+++ src/main/kotlin/com/procurement/notice/config/DaoConfiguration.kt
@@ -3,6 +3,7 @@ package com.procurement.notice.config
 import com.datastax.driver.core.Cluster
 import com.datastax.driver.core.PlainTextAuthProvider
 import com.datastax.driver.core.Session
+import com.procurement.notice.config.properties.CassandraProperties
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.ComponentScan
