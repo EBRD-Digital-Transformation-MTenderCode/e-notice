7f9ce82b7513682ebc1dfccec5a147dc63245f90
Evgen Kopach <zkopach@gmail.com>
2020-02-27 18:38:46 +0200

    ES-2520 Created De/Serializer for ApiVersion2 class

---
+++ src/main/kotlin/com/procurement/notice/infrastructure/bind/apiversion/ApiVersion2Serializer.kt
@@ -0,0 +1,19 @@
+package com.procurement.notice.infrastructure.bind.apiversion
+
+import com.fasterxml.jackson.core.JsonGenerator
+import com.fasterxml.jackson.core.JsonProcessingException
+import com.fasterxml.jackson.databind.JsonSerializer
+import com.fasterxml.jackson.databind.SerializerProvider
+import com.procurement.notice.infrastructure.dto.ApiVersion2
+import java.io.IOException
+
+class ApiVersion2Serializer : JsonSerializer<ApiVersion2>() {
+    companion object {
+        fun serialize(apiVersion: ApiVersion2): String = "${apiVersion.major}.${apiVersion.minor}.${apiVersion.patch}"
+    }
+
+    @Throws(IOException::class, JsonProcessingException::class)
+    override fun serialize(apiVersion: ApiVersion2, jsonGenerator: JsonGenerator, provider: SerializerProvider) {
+        jsonGenerator.writeString(serialize(apiVersion))
+    }
+}
