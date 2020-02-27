74b2ab3032b043122c8d06b8ae9af04958906276
Evgen Kopach <zkopach@gmail.com>
2020-02-27 18:44:37 +0200

    ES-2520 Use bean of ObjectMapper for utils method

--- src/main/kotlin/com/procurement/notice/utils/utils.kt
+++ src/main/kotlin/com/procurement/notice/utils/utils.kt
@@ -1,60 +1,23 @@
 package com.procurement.notice.utils
 
 import com.fasterxml.jackson.core.JsonProcessingException
-import com.fasterxml.jackson.databind.DeserializationFeature
 import com.fasterxml.jackson.databind.JsonNode
 import com.fasterxml.jackson.databind.ObjectMapper
-import com.fasterxml.jackson.databind.module.SimpleModule
-import com.fasterxml.jackson.databind.node.JsonNodeFactory
-import com.fasterxml.jackson.module.kotlin.registerKotlinModule
-import com.procurement.notice.databinding.JsonDateDeserializer
-import com.procurement.notice.databinding.JsonDateSerializer
+import com.procurement.notice.infrastructure.bind.date.JsonDateTimeFormatter
+import com.procurement.notice.infrastructure.bind.jackson.configuration
 import java.io.IOException
 import java.time.Instant
 import java.time.LocalDateTime
 import java.time.ZoneOffset
-import java.time.format.DateTimeFormatter
-import java.time.format.DateTimeFormatterBuilder
-import java.time.temporal.ChronoField
 import java.util.*
 
-
 private object JsonMapper {
-
-    val mapper: ObjectMapper = ObjectMapper().registerKotlinModule()
-    var dateTimeFormatter: DateTimeFormatter
-
-    init {
-        val module = SimpleModule()
-        module.addSerializer(LocalDateTime::class.java, JsonDateSerializer())
-        module.addDeserializer(LocalDateTime::class.java, JsonDateDeserializer())
-
-        mapper.registerModule(module)
-        mapper.configure(DeserializationFeature.USE_BIG_INTEGER_FOR_INTS, true)
-        mapper.configure(DeserializationFeature.USE_BIG_DECIMAL_FOR_FLOATS, true)
-        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
-        mapper.configure(DeserializationFeature.FAIL_ON_NUMBERS_FOR_ENUMS, true)
-
-        mapper.nodeFactory = JsonNodeFactory.withExactBigDecimals(true)
-
-        dateTimeFormatter = DateTimeFormatterBuilder()
-                .parseCaseInsensitive()
-                .append(DateTimeFormatter.ISO_LOCAL_DATE)
-                .appendLiteral('T')
-                .appendValue(ChronoField.HOUR_OF_DAY, 2)
-                .appendLiteral(':')
-                .appendValue(ChronoField.MINUTE_OF_HOUR, 2)
-                .optionalStart()
-                .appendLiteral(':')
-                .appendValue(ChronoField.SECOND_OF_MINUTE, 2)
-                .appendLiteral('Z')
-                .toFormatter()
-    }
+    val mapper: ObjectMapper = ObjectMapper().apply { configuration() }
 }
 
 /*Date utils*/
 fun String.toLocalDateTime(): LocalDateTime {
-    return LocalDateTime.parse(this, JsonMapper.dateTimeFormatter)
+    return LocalDateTime.parse(this, JsonDateTimeFormatter.formatter)
 }
 
 fun Long.toLocalDateTime(): LocalDateTime {
@@ -100,4 +63,18 @@ fun <T> toObject(clazz: Class<T>, json: JsonNode): T {
     } catch (e: IOException) {
         throw IllegalArgumentException(e)
     }
+}
+
+fun <T : Any> JsonNode.toObject(target: Class<T>): T {
+    try {
+        return JsonMapper.mapper.treeToValue(this, target)
+    } catch (expected: IOException) {
+        throw IllegalArgumentException("Error binding JSON to an object of type '${target.canonicalName}'.", expected)
+    }
+}
+
+fun String.toNode(): JsonNode = try {
+    JsonMapper.mapper.readTree(this)
+} catch (exception: JsonProcessingException) {
+    throw IllegalArgumentException("Error parsing String to JsonNode.", exception)
 }
