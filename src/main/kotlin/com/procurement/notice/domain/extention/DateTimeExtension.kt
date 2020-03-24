85b8dce92c3bdec58a10e8b1c17d16d0732ffb21
Evgen Kopach <zkopach@gmail.com>
2020-03-24 10:31:36 +0200

    ES-2520 Extended DateTimeExtension.kt

--- src/main/kotlin/com/procurement/notice/domain/extention/DateTimeExtension.kt
+++ src/main/kotlin/com/procurement/notice/domain/extention/DateTimeExtension.kt
@@ -1,7 +1,33 @@
 package com.procurement.notice.domain.extention
 
+import com.procurement.notice.domain.utils.Result
+import com.procurement.notice.domain.utils.Result.Companion.failure
+import com.procurement.notice.domain.utils.Result.Companion.success
 import java.time.LocalDateTime
+import java.time.ZoneId
 import java.time.ZoneOffset
-import java.time.ZonedDateTime
+import java.time.format.DateTimeFormatter
+import java.time.format.ResolverStyle
 
-fun nowDefaultUTC(): LocalDateTime = ZonedDateTime.now(ZoneOffset.UTC).toLocalDateTime()
+private const val FORMAT_PATTERN = "uuuu-MM-dd'T'HH:mm:ss'Z'"
+private val formatter: DateTimeFormatter = DateTimeFormatter.ofPattern(FORMAT_PATTERN)
+    .withResolverStyle(ResolverStyle.STRICT)
+
+fun LocalDateTime.format(): String = this.format(formatter)
+
+fun String.parse(): LocalDateTime = LocalDateTime.parse(this, formatter)
+
+fun String.tryParseLocalDateTime(): Result<LocalDateTime, String> = try {
+    success(this.parse())
+} catch (ignore: Exception) {
+    failure(FORMAT_PATTERN)
+}
+
+fun nowDefaultUTC(): LocalDateTime = LocalDateTime.now(ZoneOffset.UTC)
+
+fun LocalDateTime.convertToUTC(currentZone: ZoneId = ZoneId.systemDefault()): LocalDateTime =
+    this.atZone(currentZone).withZoneSameInstant(ZoneOffset.UTC).toLocalDateTime()
+
+fun LocalDateTime.toMilliseconds(): Long = this.atZone(ZoneOffset.systemDefault())
+    .toInstant()
+    .toEpochMilli()
