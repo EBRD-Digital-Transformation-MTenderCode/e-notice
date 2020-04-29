87de2a4b3515b4c1dc186e19238460eb5fca3cea
Yevhenii Skoryi <yevheniiskoryi@gmail.com>
2020-04-29 16:33:12 +0300

    ES-3127 added method 'generateOcid' and 'generateReleaseId' to GenerationService

--- src/main/kotlin/com/procurement/notice/application/service/GenerationService.kt
+++ src/main/kotlin/com/procurement/notice/application/service/GenerationService.kt
@@ -4,5 +4,9 @@ import java.util.*
 
 interface GenerationService {
     fun generateAmendmentId(): UUID
+
+    fun generateOcid(cpid: String, stage: String): String
+
+    fun generateReleaseId(cpid: String): String
 }
 
--- src/main/kotlin/com/procurement/notice/infrastructure/service/GenerationServiceImpl.kt
+++ src/main/kotlin/com/procurement/notice/infrastructure/service/GenerationServiceImpl.kt
@@ -1,12 +1,26 @@
 package com.procurement.notice.infrastructure.service
 
 import com.procurement.notice.application.service.GenerationService
+import com.procurement.notice.utils.milliNowUTC
 import org.springframework.stereotype.Service
 import java.util.*
 
 @Service
 class GenerationServiceImpl : GenerationService {
+
+    companion object {
+        private const val SEPARATOR = "-"
+    }
+
     override fun generateAmendmentId(): UUID {
         return UUID.randomUUID()
+    }
+
+    override fun generateOcid(cpid: String, stage: String): String {
+        return cpid + SEPARATOR + stage.toUpperCase() + SEPARATOR + milliNowUTC()
+    }
+
+    override fun generateReleaseId(cpid: String): String {
+        return cpid + SEPARATOR + milliNowUTC()
     }
 }
