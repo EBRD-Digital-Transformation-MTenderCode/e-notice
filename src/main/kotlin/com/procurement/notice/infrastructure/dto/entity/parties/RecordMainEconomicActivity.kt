69a3a48fda659b3957a9ace0b0789ca1687fbab9
katherine <kate.shaidurova@gmail.com>
2020-05-14 17:44:59 +0300

    ES-3249 Added RecordMainEconomicActivity to RecordDetails.kt

---
+++ src/main/kotlin/com/procurement/notice/infrastructure/dto/entity/parties/RecordMainEconomicActivity.kt
@@ -0,0 +1,13 @@
+package com.procurement.notice.infrastructure.dto.entity.parties
+
+import com.fasterxml.jackson.annotation.JsonInclude
+import com.fasterxml.jackson.annotation.JsonProperty
+
+class RecordMainEconomicActivity (
+    @param:JsonProperty("scheme") @field:JsonProperty("scheme") val scheme: String,
+    @param:JsonProperty("id") @field:JsonProperty("id") val id: String,
+    @param:JsonProperty("description") @field:JsonProperty("description") val description: String,
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    @param:JsonProperty("uri") @field:JsonProperty("uri") val uri: String?
+)
