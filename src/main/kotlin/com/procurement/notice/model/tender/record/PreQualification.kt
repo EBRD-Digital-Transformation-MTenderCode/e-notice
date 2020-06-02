6f327228a3ae9179c641c97e2f5354c5bc966fdc
Evgen Kopach <zkopach@gmail.com>
2020-06-02 14:44:04 +0300

    ES-3416 Added 'preQualification' object to 'createCnOnPn' request DTO.

---
+++ src/main/kotlin/com/procurement/notice/model/tender/record/PreQualification.kt
@@ -0,0 +1,18 @@
+package com.procurement.notice.model.tender.record
+
+import com.fasterxml.jackson.annotation.JsonInclude
+import com.fasterxml.jackson.annotation.JsonProperty
+import java.time.LocalDateTime
+
+data class PreQualification(
+    @field:JsonInclude(JsonInclude.Include.NON_NULL)
+    @field:JsonProperty("period") @param:JsonProperty("period") val period: Period?
+) {
+    data class Period(
+        @field:JsonInclude(JsonInclude.Include.NON_NULL)
+        @field:JsonProperty("startDate") @param:JsonProperty("startDate") val startDate: LocalDateTime?,
+
+        @field:JsonInclude(JsonInclude.Include.NON_NULL)
+        @field:JsonProperty("endDate") @param:JsonProperty("endDate") val endDate: LocalDateTime?
+    )
+}
