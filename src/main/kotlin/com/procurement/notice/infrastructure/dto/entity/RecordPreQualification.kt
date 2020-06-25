092c9077d0637fc4a3d57e3786366834f25d7f0f
yevhenii <yevheniiskoryi@gmail.com>
2020-06-25 16:30:49 +0300

    ES-3634 marked 'startDate' and 'endDate' optional in 'RecordPreQualification'

--- src/main/kotlin/com/procurement/notice/infrastructure/dto/entity/RecordPreQualification.kt
+++ src/main/kotlin/com/procurement/notice/infrastructure/dto/entity/RecordPreQualification.kt
@@ -1,5 +1,6 @@
 package com.procurement.notice.infrastructure.dto.entity
 
+import com.fasterxml.jackson.annotation.JsonInclude
 import com.fasterxml.jackson.annotation.JsonProperty
 import java.time.LocalDateTime
 
@@ -7,7 +8,9 @@ class RecordPreQualification(
     @field:JsonProperty("period") @param:JsonProperty("period") val period: Period
 ) {
     data class Period(
-        @field:JsonProperty("startDate") @param:JsonProperty("startDate") val startDate: LocalDateTime,
-        @field:JsonProperty("endDate") @param:JsonProperty("endDate") val endDate: LocalDateTime
+        @JsonInclude(JsonInclude.Include.NON_NULL)
+        @field:JsonProperty("startDate") @param:JsonProperty("startDate") val startDate: LocalDateTime?,
+        @JsonInclude(JsonInclude.Include.NON_NULL)
+        @field:JsonProperty("endDate") @param:JsonProperty("endDate") val endDate: LocalDateTime?
     )
 }
