c5d328c3f840460c62e275307242cfb9291c2528
katherine <kate.shaidurova@gmail.com>
2020-07-02 13:29:04 +0300

    ES-3594 Removed title, identifier and businessFunctions from RecordResponder.kt and RequestResponder.kt

--- src/main/kotlin/com/procurement/notice/infrastructure/dto/request/awards/RequestResponder.kt
+++ src/main/kotlin/com/procurement/notice/infrastructure/dto/request/awards/RequestResponder.kt
@@ -1,21 +1,9 @@
 package com.procurement.notice.infrastructure.dto.request.awards
 
-import com.fasterxml.jackson.annotation.JsonInclude
 import com.fasterxml.jackson.annotation.JsonProperty
 import com.procurement.notice.infrastructure.dto.entity.parties.PersonId
-import com.procurement.notice.infrastructure.dto.request.RequestBusinessFunction
-import com.procurement.notice.infrastructure.dto.request.RequestIdentifier
 
 data class RequestResponder(
     @param:JsonProperty("id") @field:JsonProperty("id") val id: PersonId,
-    @param:JsonProperty("name") @field:JsonProperty("name") val name: String,
-
-    @field:JsonInclude(JsonInclude.Include.NON_NULL)
-    @param:JsonProperty("title") @field:JsonProperty("title") val title: String?,
-
-    @field:JsonInclude(JsonInclude.Include.NON_NULL)
-    @param:JsonProperty("identifier") @field:JsonProperty("identifier") val identifier: RequestIdentifier?,
-
-    @field:JsonInclude(JsonInclude.Include.NON_EMPTY)
-    @param:JsonProperty("businessFunctions") @field:JsonProperty("businessFunctions") val businessFunctions: List<RequestBusinessFunction> = emptyList()
+    @param:JsonProperty("name") @field:JsonProperty("name") val name: String
 )
