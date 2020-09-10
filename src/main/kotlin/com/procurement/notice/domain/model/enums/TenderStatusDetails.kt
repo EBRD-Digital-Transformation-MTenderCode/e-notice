819e3cabfa4b24cfddcd914f297396e9b05a6a0d
Evgen Kopach <zkopach@gmail.com>
2020-09-10 12:23:28 +0300

    ES-4166 Extended enum TenderStatusDetails with 'aggregation' item.

--- src/main/kotlin/com/procurement/notice/domain/model/enums/TenderStatusDetails.kt
+++ src/main/kotlin/com/procurement/notice/domain/model/enums/TenderStatusDetails.kt
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonValue
 import com.procurement.notice.exception.EnumException
 
 enum class TenderStatusDetails(@JsonValue val value: String) {
+    AGGREGATION("aggregation"),
     AUCTION("auction"),
     AWARDED_CONTRACT_PREPARATION("awardedContractPreparation"),
     AWARDED_STANDSTILL("awardedStandStill"),
