b478ce454e08b9af9278628aea017cfd4f3893d3
katherine <kate.shaidurova@gmail.com>
2020-09-21 17:47:57 +0300

    ES-4257 Added CreateFeContext.kt

---
+++ src/main/kotlin/com/procurement/notice/application/service/fe/create/CreateFeContext.kt
@@ -0,0 +1,13 @@
+package com.procurement.notice.application.service.fe.create
+
+import java.time.LocalDateTime
+
+data class CreateFeContext(
+    val cpid: String,
+    val ocid: String,
+    val ocidCn: String,
+    val stage: String,
+    val releaseDate: LocalDateTime,
+    val startDate: LocalDateTime,
+    val isAuction: Boolean
+)
