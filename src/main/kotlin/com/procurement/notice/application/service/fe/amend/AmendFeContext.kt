32a8dbbc97f7e47270f4f06b55c76b9bcd3f16ab
katherine <kate.shaidurova@gmail.com>
2020-09-22 10:44:34 +0300

    ES-4272 Added AmendFeContext.kt

---
+++ src/main/kotlin/com/procurement/notice/application/service/fe/amend/AmendFeContext.kt
@@ -0,0 +1,12 @@
+package com.procurement.notice.application.service.fe.amend
+
+import java.time.LocalDateTime
+
+data class AmendFeContext(
+    val cpid: String,
+    val ocid: String,
+    val ocidCn: String,
+    val stage: String,
+    val releaseDate: LocalDateTime,
+    val startDate: LocalDateTime
+)
