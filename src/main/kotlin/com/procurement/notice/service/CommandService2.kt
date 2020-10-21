5b4d7526ca7fa267e828dc473d88e2dbae6eb764
Evgen Kopach <zkopach@gmail.com>
2020-10-21 12:35:34 +0300

    ES-4513 Added handler for 'createRecord' command.

--- src/main/kotlin/com/procurement/notice/service/CommandService2.kt
+++ src/main/kotlin/com/procurement/notice/service/CommandService2.kt
@@ -3,6 +3,7 @@ package com.procurement.notice.service
 import com.fasterxml.jackson.databind.JsonNode
 import com.procurement.notice.application.service.Logger
 import com.procurement.notice.infrastructure.dto.ApiResponse2
+import com.procurement.notice.infrastructure.handler.record.create.CreateRecordHandler
 import com.procurement.notice.infrastructure.handler.record.update.UpdateRecordHandler
 import com.procurement.notice.model.bpe.CommandType2
 import com.procurement.notice.model.bpe.errorResponse
@@ -16,6 +17,7 @@ import org.springframework.stereotype.Service
 @Service
 class CommandService2(
     private val updateRecordHandler: UpdateRecordHandler,
+    private val createRecordHandler: CreateRecordHandler,
     private val logger: Logger
 ) {
     companion object {
@@ -25,21 +27,17 @@ class CommandService2(
     fun execute(request: JsonNode): ApiResponse2 {
 
         val id = request.tryGetId()
-            .doOnError { error -> return errorResponse(fail = error, logger = logger) }
-            .get
+            .doReturn { error -> return errorResponse(fail = error, logger = logger) }
 
         val version = request.tryGetVersion()
-            .doOnError { error -> return errorResponse(fail = error, id = id, logger = logger) }
-            .get
+            .doReturn { error -> return errorResponse(fail = error, id = id, logger = logger) }
 
         val action = request.tryGetAction()
-            .doOnError { error -> return errorResponse(fail = error, id = id, version = version, logger = logger) }
-            .get
+            .doReturn { error -> return errorResponse(fail = error, id = id, version = version, logger = logger) }
 
         val response = when (action) {
-            CommandType2.UPDATE_RECORD -> {
-                updateRecordHandler.handle(node = request)
-            }
+            CommandType2.UPDATE_RECORD -> updateRecordHandler.handle(node = request)
+            CommandType2.CREATE_RECORD -> createRecordHandler.handle(node = request)
         }
 
         if (log.isDebugEnabled)
