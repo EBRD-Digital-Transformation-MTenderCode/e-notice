0c09e7299c925a5a955f7bc48f1162625b3ffc17
Evgen Kopach <zkopach@gmail.com>
2020-10-28 18:37:35 +0200

    ES-4543 Marked unit's id as mandatory.

--- src/main/kotlin/com/procurement/notice/infrastructure/dto/request/tender/RequestUnit.kt
+++ src/main/kotlin/com/procurement/notice/infrastructure/dto/request/tender/RequestUnit.kt
@@ -6,8 +6,7 @@ import com.procurement.notice.model.ocds.Value
 
 data class RequestUnit(
 
-    @field:JsonInclude(JsonInclude.Include.NON_NULL)
-    @field:JsonProperty("id") @param:JsonProperty("id") val id: String?,
+    @field:JsonProperty("id") @param:JsonProperty("id") val id: String,
 
     @field:JsonInclude(JsonInclude.Include.NON_NULL)
     @field:JsonProperty("name") @param:JsonProperty("name") val name: String?,
