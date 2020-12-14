292c672db5581e8b774b29f6eafd5cbdf5852f8d
katherine <kate.shaidurova@gmail.com>
2020-12-14 14:13:35 +0200

    ES-5077 Added relatedRequirementId, period and unit to ContractTerm

--- src/main/kotlin/com/procurement/notice/model/ocds/ContractTerm.kt
+++ src/main/kotlin/com/procurement/notice/model/ocds/ContractTerm.kt
@@ -2,6 +2,7 @@ package com.procurement.notice.model.ocds
 
 import com.fasterxml.jackson.annotation.JsonCreator
 import com.fasterxml.jackson.annotation.JsonInclude
+import com.fasterxml.jackson.annotation.JsonProperty
 import java.util.*
 
 data class ContractTerm @JsonCreator constructor(
@@ -25,7 +26,16 @@ data class AgreedMetric @JsonCreator constructor(
     val description: String?,
 
     @field:JsonInclude(JsonInclude.Include.NON_EMPTY)
-    val observations: LinkedList<Observation>?
+    val observations: LinkedList<Observation>?,
+
+    @field:JsonInclude(JsonInclude.Include.NON_EMPTY)
+    @field:JsonProperty("relatedRequirementId") @param:JsonProperty("relatedRequirementId") var relatedRequirementId: String? = null,
+
+    @field:JsonInclude(JsonInclude.Include.NON_NULL)
+    @field:JsonProperty("period") @param:JsonProperty("period") val period: Period? = null,
+
+    @field:JsonInclude(JsonInclude.Include.NON_NULL)
+    @field:JsonProperty("unit") @param:JsonProperty("unit") val unit: Unit? = null
 )
 
 data class Observation @JsonCreator constructor(
